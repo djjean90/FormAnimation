fb3936320b0ede9dff555e274b54f0cc786864c2
djjean <djjean90@gmail.com>
2021-10-10 22:29:47 +0300

    final

---
+++ script.js
@@ -0,0 +1,13 @@
+//Script.js
+
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, ind) =>
+        `<span style="transition-delay:${ind * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
